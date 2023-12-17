cfe4e5636a265cd157357d0709f48abed9e82faf
Tatsuya <tatsu.mase@icloud.com>
2023-12-18 01:31:27 +0900

    promptの調整（Zeroshot-CoTの追加など）length.txtに文章の文字数の保存

--- main.py
+++ main.py
@@ -65,75 +65,138 @@ def encode_image(image_path):
         return base64.b64encode(image_file.read()).decode('utf-8')
     
 
-def gpt_4_turbo():
+def baseline():
+    print("baseline")
 
-    with open("./gpt-4-trubo/system_prompts/young.txt") as f:
+    with open("./baseline/system_prompts/young.txt") as f:
         system_prompt_young = f.read()
 
-    with open("./gpt-4-trubo/system_prompts/elderly.txt") as f:
+    with open("./baseline/system_prompts/elderly.txt") as f:
         system_prompt_elderly = f.read()
 
+    with open("./baseline/length.txt", mode = "w") as length_file:
 
-    for item in items:
-        with open("./product_info/" + item + '.txt', mode='r') as g:
-            contents_sheet = g.read()
+        for item in items:
+            with open("./product_info/" + item + '.txt', mode='r') as g:
+                contents_sheet = g.read()
 
-        res_young = get_response_gpt_4_turbo(system_prompt_young, contents_sheet)
-        print(len(res_young))
+            # while(True):
+            #     res_young = get_response_gpt_4_turbo(system_prompt_young, contents_sheet)
+            #     if 350 < len(res_young) < 450:
+            #         break
+            res_young = get_response_gpt_4_turbo(system_prompt_young, contents_sheet)
 
-        with open("./gpt-4-trubo/description/young_" + item + '.txt', mode="w") as g:
-            g.write(res_young)
+            length_file.write('young_' + item + ': ' + str(len(res_young)) + '\n')
+            print("young_" + item +': ', len(res_young))
+            with open("./baseline/description/young_" + item + '.txt', mode="w") as g:
+                g.write(res_young)
 
-        res_elderly = get_response_gpt_4_turbo(system_prompt_elderly, contents_sheet)
-        print(len(res_elderly))
+            # while(True):
+            #     res_elderly = get_response_gpt_4_turbo(system_prompt_elderly, contents_sheet)
+            #     if 350 < len(res_elderly) < 450:
+            #         break
+            res_elderly = get_response_gpt_4_turbo(system_prompt_elderly, contents_sheet)
 
-        with open("./gpt-4-trubo/description/elderly_" + item + '.txt', mode="w") as g:
-            g.write(res_elderly)
+            length_file.write('elderly_' + item + ': ' + str(len(res_elderly)) + '\n')
+            print("elderly_" + item +': ', len(res_elderly))
+            with open("./baseline/description/elderly_" + item + '.txt', mode="w") as g:
+                g.write(res_elderly)    
 
 
-def gpt_4_with_vision():
-    with open("./gpt-4-with-vision/system_prompts/young.txt") as f:
+def Zeroshot_CoT():
+    print("Zeroshot_CoT")
+
+    with open("./Zeroshot-CoT/system_prompts/young.txt") as f:
         system_prompt_young = f.read()
 
-    with open("./gpt-4-with-vision/system_prompts/elderly.txt") as f:
+    with open("./Zeroshot-CoT/system_prompts/elderly.txt") as f:
         system_prompt_elderly = f.read()
 
-    for item in items:
-        with open("./product_info/" + item + ".txt") as g:
-            contents_sheet = g.read()
+    with open("./Zeroshot-CoT/length.txt", mode = "w") as length_file:
 
-        # Path to your image
-        image_path = "./product_info/" + item + ".jpg"
+        for item in items:
+            with open("./product_info/" + item + '.txt', mode='r') as g:
+                contents_sheet = g.read()
 
-        # Getting the base64 string
-        base64_image = encode_image(image_path)
+            # while(True):
+            #     res_young = get_response_gpt_4_turbo(system_prompt_young, contents_sheet)
+            #     if 350 < len(res_young) < 450:
+            #         break
+            res_young = get_response_gpt_4_turbo(system_prompt_young, contents_sheet)
+
+            length_file.write('young_' + item + ': ' + str(len(res_young)) + '\n')
+            print("young_" + item +': ', len(res_young))
+            with open("./Zeroshot-CoT/description/young_" + item + '.txt', mode="w") as g:
+                g.write(res_young)
+
+            # while(True):
+            #     res_elderly = get_response_gpt_4_turbo(system_prompt_elderly, contents_sheet)
+            #     if 350 < len(res_elderly) < 450:
+            #         break
+            res_elderly = get_response_gpt_4_turbo(system_prompt_elderly, contents_sheet)
+
+            length_file.write('elderly_' + item + ': ' + str(len(res_elderly)) + '\n')
+            print("elderly_" + item +': ', len(res_elderly))
+            with open("./Zeroshot-CoT/description/elderly_" + item + '.txt', mode="w") as g:
+                g.write(res_elderly)    
+
+
+def with_vision():
+    print("with_vision")
+
+    with open("./with-vsion/system_prompts/young.txt") as f:
+        system_prompt_young = f.read()
+
+    with open("./with-vsion/system_prompts/elderly.txt") as f:
+        system_prompt_elderly = f.read()
     
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {api_key}"
-        }
+    with open("./with-vsion/length.txt", mode = "w") as length_file:
 
+        for item in items:
+            with open("./product_info/" + item + ".txt") as g:
+                contents_sheet = g.read()
+
+            # Path to your image
+            image_path = "./product_info/" + item + ".jpg"
+
+            # Getting the base64 string
+            base64_image = encode_image(image_path)
         
-        res_young = get_response_gpt_4_with_vision(system_prompt_young, contents_sheet, base64_image, headers)
-        print(len(res_young))
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {api_key}"
+            }
 
-        with open("./gpt-4-with-vision/description/young_" + item + ".txt", mode="w") as g:
-            g.write(res_young)
+            
+            res_young = get_response_gpt_4_with_vision(system_prompt_young, contents_sheet, base64_image, headers)
+            
+            length_file.write('young_' + item + ': ' + str(len(res_young)) + '\n')
+            print("young_" + item +': ', len(res_young))
+            with open("./with-vsion/description/young_" + item + ".txt", mode="w") as g:
+                g.write(res_young)
 
-        res_elderly = get_response_gpt_4_with_vision(system_prompt_elderly, contents_sheet, base64_image, headers)
-        print(len(res_elderly))
-
-        with open("./gpt-4-with-vision/description/elderly_" + item + ".txt", mode="w") as g:
-            g.write(res_elderly)
+            res_elderly = get_response_gpt_4_with_vision(system_prompt_elderly, contents_sheet, base64_image, headers)
+            
+            length_file.write('elderly_' + item + ': ' + str(len(res_elderly)) + '\n')
+            print("elderly_" + item +': ', len(res_elderly))
+            with open("./with-vsion/description/elderly_" + item + ".txt", mode="w") as g:
+                g.write(res_elderly)
 
 
 def main(folder):
+    if folder == "all":
+        baseline()
+        Zeroshot_CoT()
+        with_vision()
 
-    if folder == "gpt-4-turbo":
-        gpt_4_turbo()
+    elif folder == "baseline":
+        baseline()
 
-    if folder == "gpt-4-with-vision":
-        gpt_4_with_vision()
+    elif folder == "Zeroshot-CoT":
+        Zeroshot_CoT()
+
+    elif folder == "with-vsion":
+        with_vision()
 
     else:
         print("Usage: python3 main.py folder_name")
